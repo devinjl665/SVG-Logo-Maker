22173dc3a1c2dcd5c1d0f02a2d99062a75033b22
elroy <devinjl665@gmail.com>
2024-03-07 13:30:02 -0600

    Made adjustments to code so that tests pass

--- index.js
+++ index.js
@@ -1,68 +1,58 @@
-const inquirer = ('inquirer');
+const inquirer = require('inquirer');
 const fs = require('fs');
 const { Circle, Triangle, Square } = require('./lib/shapes');
 
-
 const logoPrompts = [
-
-    'Enter text for you logo (Up to three characters.)',
-
-
-    'Select the color of your text (Enter a color or the hex code).',
-
-
-    'Select your logo shape',
-
-
-    'Select the color of the logo shape (Enter a color or the hex code).',
-
+  'Enter text for your logo (Up to three characters.)',
+  'Select the color of your text (Enter a color or the hex code).',
+  'Select your logo shape',
+  'Select the color of the logo shape (Enter a color or the hex code).',
 ];
 
-
 function initPrompts() {
-    inquirer
+  inquirer
     .prompt([
-        {
-            type: 'input',
-            message: logoPrompts[0],
-            name: 'text',
-        },
-        {
-            type: 'input',
-            message: logoPrompts[1],
-            name: 'textColor',
-        },
-        {
-            type: 'list',
-            message: logoPrompts[2],
-            choices: ['Circle', 'Triangle', 'Square' ],
-            name: 'shape',
-        },
-        {
-            type: 'input',
-            message: logoPrompts[3],
-            name: 'shapeColor',
-        },
-    ]).then((answers) => {
-        var shape ='';
+      {
+        type: 'input',
+        message: logoPrompts[0],
+        name: 'text',
+      },
+      {
+        type: 'input',
+        message: logoPrompts[1],
+        name: 'textColor',
+      },
+      {
+        type: 'list',
+        message: logoPrompts[2],
+        choices: ['Circle', 'Triangle', 'Square'],
+        name: 'shape',
+      },
+      {
+        type: 'input',
+        message: logoPrompts[3],
+        name: 'shapeColor',
+      },
+    ])
+    .then((answers) => {
+      let shape = '';
 
-        if (answers.shape == 'Circle') {
-            shape = new Circle(answers.text, answers.textColor, answers.shapeColor)    
-        } else if (answers.shape == 'Triangle') {
-            shape = new Triangle(answers.text, answers.textColor, answers.shapeColor)
-        } else {
-            shape = new Square(answers.text, answers.textColor, answers.shapeColor)
-        }
+      if (answers.shape === 'Circle') {
+        shape = new Circle(answers.text, answers.textColor, answers.shapeColor);
+      } else if (answers.shape === 'Triangle') {
+        shape = new Triangle(answers.text, answers.textColor, answers.shapeColor);
+      } else {
+        shape = new Square(answers.text, answers.textColor, answers.shapeColor);
+      }
 
-        shape.createShape();
+      shape.createShape();
 
-        const html = shape.render();
+      const html = shape.render();
 
-
-        fs.createFile('./Examples/logo.svg', html, (err) =>
-            err ? console.log(err) : console.log('Logo generated!'))
-    })
+      fs.writeFile('./Examples/logo.svg', html, (err) =>
+        err ? console.log(err) : console.log('Logo generated!')
+      );
+    });
 }
-
 
 initPrompts();
